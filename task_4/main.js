6395a3af84bb67d3fac4a50a3feac3a44bd011f3
TwilDust <thecrecker666@gmail.com>
2024-02-12 10:25:59 +0300

    task 4

---
+++ task_4/main.js
@@ -0,0 +1,36 @@
+// const students = ["Tom", "Bob", "Hurry", "Ron", "Stan"];
+
+// for (let i = 0; i < students.length; i++)
+// {
+//     let result = students[i];
+//     console.log(result);
+// }
+
+// let people = [["Tom", 18],["Bob", 17],["Stan", 36]];
+
+// console.log("|-----------------------------|");
+// for (let i = 0; i < people.length; i++)
+// {
+//     for (let j = 0; j < people[i].length; j++)
+//     {
+//         let result = people[i][j];
+//         console.log(result);
+//     }
+//     console.log("|-----------------------------|");
+// }
+
+//  const students = ["Tom", "Bob", "Hurry", "Ron"];
+
+// // in - индексы; of - элементы
+
+// for (let item of students)
+// {
+//     console.log(item);
+// }
+
+const person = {name: "Tom", age: 23};
+
+for (item in person)
+{
+    console.log(item, person[item]);
+}
